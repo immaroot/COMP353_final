655a27fcc55f9e3b8048a241c3d914be347790dc
Frank <frank.sorschak@gmail.com>
2020-08-09 23:07:25 -0400

    dynamic application button based on app status

--- final/resources/views/job_seeker/dashboard.blade.php
+++ final/resources/views/job_seeker/dashboard.blade.php
@@ -33,7 +33,13 @@
                     <td>{{ $details[$i] -> created_at}}</td>
                     <td>{{ $applications[$i] -> created_at}}</td>
                     <td><a class="btn btn-primary" href="">View Job Post</a></td>
+                    @if ($applications[$i] -> status == 1)
+                    <td><h2>You've been hired!</h2></td>
+                    @elseif ($applications[$i] -> status == 2)
+                    <td><a class = "btn btn-secondary" href="{{url('job_seeker/application/'.$applications[$i]->id.'/delete')}}">You've been denied.. click to remove</a></td>
+                    @else
                     <td><a class="btn btn-secondary" href="{{url('job_seeker/application/'.$details[$i]->id)}}">Modify Application</a></td>
+                    @endif
                 </tr>
                 @endfor
                 </tbody>
